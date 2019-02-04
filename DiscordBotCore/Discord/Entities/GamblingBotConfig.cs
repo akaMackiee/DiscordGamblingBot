2f3257e9b7fcb49f174e07e6ca3f9296ffc73980
akaMackiee <46831591+akaMackiee@users.noreply.github.com>
2019-02-04 22:38:35 +0100

    Update

---
+++ DiscordBotCore/Discord/Entities/GamblingBotConfig.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscordBotCore.Discord.Entities
+{
+    class GamblingBotConfig
+    {
+    }
+}
---
+++ DiscordBotCore/Discord/SocketConfig.cs
@@ -0,0 +1,22 @@
+﻿using Discord.WebSocket;
+using Discord;
+using System;
+
+namespace DiscordBotCore.Discord
+{
+    public static class SocketConfig
+    {
+        public static DiscordSocketConfig GetDefault()
+        {
+            return new DiscordSocketConfig();
+            {
+                LogLevel = LogSeverity.Verbose;
+            }
+        }
+
+        public static DiscordSocketConfig Generate()
+        {
+            return new DiscordSocketConfig();
+        }
+    }
+}
