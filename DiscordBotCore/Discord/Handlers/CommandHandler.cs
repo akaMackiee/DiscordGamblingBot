c666426222d43137096838f854ce5402da81281e
akaMackiee <46831591+akaMackiee@users.noreply.github.com>
2019-02-15 22:26:55 +0100

    Create CommandHandler.cs

---
+++ DiscordBotCore/Discord/Handlers/CommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+using DiscordBotCore.Discord.Entities;
+
+namespace DiscordBotCore.Discord.Handlers
+{
+    class CommandHandler
+    {
+        DiscordSocketClient _client;
+        DiscordLogger _logger;
+
+        public CommandHandler(DiscordSocketClient client, DiscordLogger logger)
+        {
+            _client = client;
+            _logger = logger;
+        }
+
+        internal async Task CommandHandlerAsync(GamblingBotConfig config)
+        {
+
+        }
+    }
+}
