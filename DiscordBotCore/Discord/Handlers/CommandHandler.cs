0a36acd1dd89596675f74dfd9d9335a188b5dac8
akaMackiee <46831591+akaMackiee@users.noreply.github.com>
2019-02-15 22:35:08 +0100

    Update CommandHandler.cs

--- DiscordBotCore/Discord/Handlers/CommandHandler.cs
+++ DiscordBotCore/Discord/Handlers/CommandHandler.cs
@@ -2,6 +2,9 @@
 using Discord.WebSocket;
 using System.Threading.Tasks;
 using DiscordBotCore.Discord.Entities;
+using Discord.Commands;
+using System.Reflection;
+using System;
 
 namespace DiscordBotCore.Discord.Handlers
 {
@@ -9,16 +12,39 @@ namespace DiscordBotCore.Discord.Handlers
     {
         DiscordSocketClient _client;
         DiscordLogger _logger;
+        CommandService _service;
+        GamblingBotConfig _config;
 
-        public CommandHandler(DiscordSocketClient client, DiscordLogger logger)
+        public CommandHandler(DiscordSocketClient client, DiscordLogger logger, CommandService service, GamblingBotConfig config)
         {
             _client = client;
             _logger = logger;
+            _service = service;
+            _config = config;
         }
 
-        internal async Task CommandHandlerAsync(GamblingBotConfig config)
+        public async Task InitializeAsync()
         {
+            await _service.AddModulesAsync(Assembly.GetEntryAssembly(), null);
+            _client.MessageReceived += HandleCommandAsync;
+        }
 
+        internal async Task HandleCommandAsync(SocketMessage s)
+        {
+            var msg = s as SocketUserMessage;
+            if (msg == null) return;
+
+            var context = new SocketCommandContext(_client, msg);
+            int argPos = 0;
+            if (msg.HasStringPrefix(_config.Prefix, ref argPos)
+                || msg.HasMentionPrefix(_client.CurrentUser, ref argPos))
+            {
+                var result = await _service.ExecuteAsync(context, argPos, null);
+                if (!result.IsSuccess && result.Error != CommandError.UnknownCommand)
+                {
+                    Console.WriteLine(result.ErrorReason);
+                }
+            }
         }
     }
 }
